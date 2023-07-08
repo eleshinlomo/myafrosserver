3a01eb09543de98c531697ed4f7056d5b6440a31
eleshinlomo <seun.olatunji2@gmail.com>
2023-07-08 18:17:43 -0400

    m

--- myafros_project/settings.py
+++ myafros_project/settings.py
@@ -136,7 +136,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
 
 
 
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
