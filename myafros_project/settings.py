9c76a10f161651aa0b6fe442752f80e5597dc3fd
eleshinlomo <seun.olatunji2@gmail.com>
2023-07-08 10:33:57 -0400

    dtbs provisioning

--- myafros_project/settings.py
+++ myafros_project/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
 
@@ -129,7 +129,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 
 
