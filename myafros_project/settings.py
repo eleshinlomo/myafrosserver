22f84587177519c1f76122b413233de4d3678a4b
eleshinlomo <seun.olatunji2@gmail.com>
2023-07-09 06:28:57 -0400

    media

--- myafros_project/settings.py
+++ myafros_project/settings.py
@@ -129,10 +129,10 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS =  [
     os.path.join(BASE_DIR, 'static')
                      ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
+STATIC_ROOT = BASE_DIR / 'staticfiles_build'
 
 
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
--- myafros_project/urls.py
+++ myafros_project/urls.py
@@ -14,9 +14,11 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls'))
 ]
 
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
 
 
 
