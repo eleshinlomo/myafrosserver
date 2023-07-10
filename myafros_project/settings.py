610bde54b33856c7bb76d2adaacbb7e4d833205e
eleshinlomo <seun.olatunji2@gmail.com>
2023-07-10 14:50:41 -0400

    route config

--- myafros_project/settings.py
+++ myafros_project/settings.py
@@ -154,7 +154,6 @@ ALLOWED_HOSTS = [
 CORS_ALLOWED_ORIGINS = [
 'http://localhost:3000',
 'http://127.0.0.1:8000',
-'https://myafross.netlify.app',
 'https://myafrosclient.vercel.app',
 'https://myafros.com'
 
