9b3e3fb72b571df74614f425bdd26d0f75053b4c
eleshinlomo <seun.olatunji2@gmail.com>
2023-07-11 08:04:01 -0400

    m

--- myafros_project/settings.py
+++ myafros_project/settings.py
@@ -160,7 +160,7 @@ ALLOWED_HOSTS = [
 CORS_ALLOWED_ORIGINS = [
 'http://localhost:3000',
 'http://127.0.0.1:8000',
-'https://myafrosfrontend.vercel.app',
+'https://myafrosclient.vercel.app',
 'https://myafros.com',
 'https://www.myafros.com'
 
